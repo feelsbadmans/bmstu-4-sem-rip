7b32b5d33675cb2e027eeb2666bb29a06fd508ea
Alexey <grysinn@yandex.ru>
2021-09-12 22:03:37 +0300

    fixed lab2 task

--- lab2/main.py
+++ lab2/main.py
@@ -1,15 +1,22 @@
 from lab_python_oop.rectangle import Rectangle
 from lab_python_oop.circle import Circle
 from lab_python_oop.square import Square
+from logbook import Logger, StreamHandler
+import sys
 
 
 def main():
-    r = Rectangle("синего", 4, 3)
-    c = Circle("зеленого", 11)
-    s = Square("красного", 22)
-    print(r)
-    print(c)
-    print(s)
+    rectangle = Rectangle("синего", 6, 7)
+    circle = Circle("зеленого", 6)
+    square = Square("красного", 6)
+    StreamHandler(sys.stdout).push_application()
+    log = Logger('Фигура')
+    print(rectangle)
+    log.info(rectangle.FIGURE_TYPE)
+    print(circle)
+    log.info(circle.FIGURE_TYPE)
+    print(square)
+    log.info(square.FIGURE_TYPE)
 
 if __name__ == "__main__":
     main()
