aa7dd5c2f91a27e6c53712744757d01672cc0bb6
feelsbadmans <grysinn@yandex.ru>
2021-11-13 14:14:37 +0300

    done lab5

--- lab5/django_orm/master/views.py
+++ lab5/django_orm/master/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Pc, Processor
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def pcs(request):
+    pcs = Pc.objects.all()
+    return render(request, 'pcs.html', {'pcs': pcs})
+
+def pc_detail(request, id):
+    pc = Pc.objects.get(id=id)
+    proc = Processor.objects.get(id=pc.proc_id)
+    return render(request, 'pc_detail.html', {'pc': pc, 'proc': proc})
+
+def processors(request):
+    processors = Processor.objects.all()
+    return render(request, 'processors.html', {'processors': processors})
+
+def proc_detail(request, id):
+    proc = Processor.objects.get(id=id)
+    return render(request, 'proc_detail.html', {'proc': proc})
